580d4b73189536e6e73f911521b13d6742ab2d42
Ladislav Borzensky <ladislav.borzensky@ferratum.com>
2017-06-05 21:57:13 +0200

    Test

--- frame-persist/src/main/java/sk/loveit/frame/persist/repository/BaseEntityRepository.java
+++ frame-persist/src/main/java/sk/loveit/frame/persist/repository/BaseEntityRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.repository.CrudRepository;
 import sk.loveit.frame.persist.entity.BaseEntity;
 
 public interface BaseEntityRepository<T extends BaseEntity> extends CrudRepository<T, String> {
-	
 }
